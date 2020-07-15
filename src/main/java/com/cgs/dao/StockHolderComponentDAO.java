5c8c17c3f691c87179078b969bab679cb517c725
caoguangshu <caoguangshu@corp.netease.com>
2020-07-15 16:57:19 +0800

    提交代码

--- src/main/java/com/cgs/dao/StockHolderComponentDAO.java
+++ src/main/java/com/cgs/dao/StockHolderComponentDAO.java
@@ -1,9 +1,6 @@
 package com.cgs.dao;
 
-import org.apache.ibatis.annotations.Insert;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface StockHolderComponentDAO {
@@ -12,5 +9,6 @@ public interface StockHolderComponentDAO {
 
     String COLUMNS = " stock_id, organization_type, positions, number_of_shares_held," +
             " proportion_of_tradable_shares, proportion_in_total_equity, release_date ";
+
 
 }
