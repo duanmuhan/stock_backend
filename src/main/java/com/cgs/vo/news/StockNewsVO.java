a6700719c673942282a74b8884482926d25da750
caoguangshu <caoguangshu@corp.netease.com>
2020-08-01 20:36:05 +0800

    提交代码

---
+++ src/main/java/com/cgs/vo/news/StockNewsVO.java
@@ -0,0 +1,12 @@
+package com.cgs.vo.news;
+
+import lombok.Data;
+
+@Data
+public class StockNewsVO {
+    private String title;
+    private String targetPlate;
+    private String targetPlateId;
+    private String source;
+    private String platform;
+}
