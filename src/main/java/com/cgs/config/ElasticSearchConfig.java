059a0c82fb874ade2cc2fd5c72dc05c297caf441
caoguangshu <caoguangshu@corp.netease.com>
2021-02-09 03:01:47 +0800

    feature:提交es的依赖

---
+++ src/main/java/com/cgs/config/ElasticSearchConfig.java
@@ -0,0 +1,13 @@
+package com.cgs.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author caoguangshu
+ * @date 2021/2/9
+ * @time 2:47 上午
+ */
+@Configuration
+public class ElasticSearchConfig {
+
+}
